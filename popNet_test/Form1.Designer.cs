e9046203610aaacccdedc1566b8e427d43519e70
Mega-Gorilla <hahahadesu@gmail.com>
2024-05-16 10:33:02 +0900

    プロジェクト ファイルを追加します。

---
+++ popNet_test/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace popNet_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelHostname = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Btn_Path = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(617, 732);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "受信";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelHostname
+            // 
+            this.labelHostname.AutoSize = true;
+            this.labelHostname.Location = new System.Drawing.Point(12, 46);
+            this.labelHostname.Name = "labelHostname";
+            this.labelHostname.Size = new System.Drawing.Size(43, 18);
+            this.labelHostname.TabIndex = 1;
+            this.labelHostname.Text = "Path";
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(73, 90);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(694, 618);
+            this.listView1.TabIndex = 12;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 18);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Result";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(73, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(613, 25);
+            this.textBox1.TabIndex = 14;
+            // 
+            // Btn_Path
+            // 
+            this.Btn_Path.Location = new System.Drawing.Point(692, 38);
+            this.Btn_Path.Name = "Btn_Path";
+            this.Btn_Path.Size = new System.Drawing.Size(75, 35);
+            this.Btn_Path.TabIndex = 15;
+            this.Btn_Path.Text = "参照";
+            this.Btn_Path.UseVisualStyleBackColor = true;
+            this.Btn_Path.Click += new System.EventHandler(this.Btn_Path_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(817, 787);
+            this.Controls.Add(this.Btn_Path);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.labelHostname);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelHostname;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Btn_Path;
+    }
+}
+
---
+++ popNet_test/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System.IO;
+using System.Threading;
+
+namespace popNet_test
+{
+    public partial class Form1 : Form
+    {
+        private const string ApplicationName = "Stock BOX";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // ListViewの設定
+            listView1.View = View.Details;
+            listView1.Columns.Add("Subject", 400);
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {   // スコープ設定で指定したURLを追加
+                string[] Scopes = { GmailService.Scope.GmailReadonly };
+                UserCredential credential;
+
+                // Token の取得
+                using (var stream = new FileStream(textBox1.Text, FileMode.Open, FileAccess.Read))
+                {
+                    string credPath = "token.json";
+                    credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                        GoogleClientSecrets.FromStream(stream).Secrets,
+                        Scopes,
+                        "user",
+                        CancellationToken.None,
+                        new FileDataStore(credPath, true));
+                }
+
+                await FetchEmails(credential);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+        private async Task FetchEmails(UserCredential credential)
+        {
+            try
+            {
+                var service = new GmailService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = ApplicationName,
+                });
+
+                // ユーザーのメールリストを取得
+                var request = service.Users.Messages.List("me");
+                request.LabelIds = "INBOX";
+                request.MaxResults = 10;
+
+                var response = await request.ExecuteAsync();
+                listView1.Items.Clear();
+
+                if (response.Messages != null && response.Messages.Count > 0)
+                {
+                    foreach (var msg in response.Messages)
+                    {
+                        var messageRequest = service.Users.Messages.Get("me", msg.Id);
+                        var message = await messageRequest.ExecuteAsync();
+
+                        string subject = message.Payload.Headers.FirstOrDefault(h => h.Name == "Subject")?.Value;
+
+                        // リストビューに表示
+                        ListViewItem item = new ListViewItem(subject);
+                        listView1.Items.Add(item);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("No messages found.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+
+        private void Btn_Path_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "JSON files (*.json)|*.json|All files (*.*)|*.*";
+                openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // 選択したファイルパスをテキストボックスに表示
+                    textBox1.Text = openFileDialog.FileName;
+                }
+            }
+        }
+    }
+}
